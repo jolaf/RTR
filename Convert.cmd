19c7f61eca1716dd9fe75e5fa6f3cef7c019b7bf
Vasily Zakharov <vmzakhar@gmail.com>
2020-03-30 18:14:58 +0300

    Decreased FPS to 15 to avoid extra-fast playback

--- Convert.cmd
+++ Convert.cmd
@@ -1,2 +1,2 @@
 @echo on
-ffmpeg -r 30 -i VESAFILM.PNG -c:v libx264 -pix_fmt yuv420p -preset veryslow -crf 17 -tune zerolatency -movflags +faststart -y VESAFILM-H264.mp4
+ffmpeg -r 15 -i VESAFILM.PNG -c:v libx264 -pix_fmt yuv420p -preset veryslow -crf 17 -tune zerolatency -movflags +faststart -y VESAFILM-H264.mp4
